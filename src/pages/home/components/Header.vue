7519776703592736ba594c54764abb6c9af965c4
FrostedMonkey <35445416+FrostedMonkey@users.noreply.github.com>
2020-04-02 19:24:25 +0800

    123
    123

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="header-input">
             <span class="iconfont">&#xe61c;</span>
-            输入城市/景点/游玩/主题
+            输入城市/景点/游玩/主题。。。。
         </div>
         <div class="header-right">
             城市
